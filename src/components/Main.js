8b591bc2c57ec01834f534a80a8570b9696565a6
Koski Jenna <s2200123@BC-C02GR0BPQ6LR-MacBook-Air.local>
2022-04-25 14:57:46 +0300

    created the components

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,15 +4,18 @@ import FavList from "./FavList";
 import PokeList from "./PokeList";
 import About from "./About";
 import { Route, Routes } from "react-router-dom";
+import PokeSingle from "./PokeSingle";
 
 const Main = () => {
   return (
     <main>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/pokelist" element={<PokeList />} />
-        <Route path="/favlist" element={<FavList />} />
-        <Route path="/about" element={<About />} />
+        <Route path="pokelist" element={<PokeList />}>
+          <Route path="pokesingle" element={<PokeSingle />} />
+        </Route>
+        <Route path="favlist" element={<FavList />} />
+        <Route path="about" element={<About />} />
       </Routes>
     </main>
   );
--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 const PokeList = () => {
-  return <div>PokeList</div>;
+  return (
+    <div>
+      PokeList
+      <Link to="pokesingle">Show me single Pokemon</Link>
+      <Outlet />
+    </div>
+  );
 };
 
 export default PokeList;
